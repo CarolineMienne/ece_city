c89776d090d3f1ef943e2be9f05560bbc3118f7e
Baltair <julbrehin@gmail.com>
2015-03-28 22:06:56 +0100

    Added first board related functions + Added structure/type declaration of t_board + Added board and grid constructors + Added first game initialization constant headers

--- src/board.c
+++ src/board.c
@@ -1,1 +1,70 @@
 #include "board.h"
+
+/************************
+ * 	Global functions	*
+ ************************/
+
+/** Board constructor **/
+t_board boardConstruct()
+{
+	int x,y;
+	t_board* board = NULL;
+
+	board = (t_board*) malloc(sizeof(t_board));
+	if (board == NULL)
+	{
+		fprintf(stderr, "Error while allocating board instance !\nProgram must quit\n");
+		exit(EXIT_FAILURE);
+	}
+
+	/* Variable initialization */
+	board->nb_inhabs 	= NB_INHABS_AT_GAME_START;
+	board->months 		= TIME_AT_START;
+	board->flouz 		= FLOUZ_AT_START;
+	board->nb_pplants 	= NB_PPLANTS_AT_START;
+	board->nb_wcastles 	= NB_WCASTLES_AT_START;
+
+	/* Structure allocations */
+	board->graph = (t_graph*) graphConstruct();
+	board->grid  = (t_grid*) gridConstruct();
+	if (board->grid == NULL)
+	{
+		fprintf(stderr, "Error while allocating game grid !\nProgram must quit");
+		exit(EXIT_FAILURE);
+	}
+
+	// Since there are no resource provider (water castles/power plants) 
+	// at the beginning we can't allocate anything.
+	// Thus, we set these lists as pointing to NULL.
+	board->water_network = NULL;
+	board->elec_network	 = NULL;
+
+	return board;
+}
+
+/** Board destructor **/
+void boardDestroy(t_board* board)
+{
+	/// @TODO Destroy properly the board instance
+}
+
+/************************
+*	Static functions	*
+************************/
+
+static t_box** gridConstruct()
+{
+	t_box** grid = NULL;
+	(t_box**) malloc(BOARD_HEIGHT * sizeof(t_box*));
+	if (grid == NULL)
+	{
+		return NULL;
+	}
+
+	for (y = 0; y < BOARD_HEIGHT; y++)
+	{
+		grid[y] = (t_box*) boxConstruct();
+	}
+
+	return grid;
+}
---
+++ src/board.h
@@ -0,0 +1,79 @@
+#ifndef ECE_CITY_BOARD_H
+#define ECE_CITY_BOARD_H
+
+/************************
+*	LIBRARY INCLUSIONS	*
+************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+
+/****************************
+*	STRUCTURE DECLARATION	*
+****************************/
+typedef struct board
+{
+	int nb_inhabs; 	// Number of inhabitants in the city
+	int months;		// Current time (in months)
+	int flouz;		// Current amount of money in the coffers of the city
+
+	/* Board related fields */
+	int nb_pplants;	// Number of power plants currently on game
+	int nb_wcastles;	// Number of water castles currently on the game
+	t_graph *graph;	// Structure containing all data related to the network graph
+	t_PB** water_network;	// Lists all water castles
+	t_PB** elec_network;	// Lists all power plants
+	t_box** grid;	// Matrix that represents the gameboard. Each index points to a box structure
+}t_board;
+
+/****************************
+*	CONSTANT DECLARATIONS	*
+****************************/
+
+#define BOARD_WIDTH		45
+#define BOARD_HEIGHT 	35
+
+#define TIME_AT_START 	0
+#define FLOUZ_AT_START 	500000
+#define NB_INHABS_AT_GAME_START 0
+#define NB_PPLANTS_AT_START 	0
+#define NB_WCASTLES_AT_START 	0
+
+
+/****************************
+*	FUNCTION DECLARATIONS	*
+****************************/
+
+/************************
+ * 	Global functions	*
+ ************************/
+
+/**
+ * Board constructor
+ * @return (t_board *) allocated board instance
+ */
+t_board* boardConstruct();
+
+/**
+ * Board destructor
+ * @param (t_board *) board instance to be destroyed
+ */
+void boardDestroy(t_board* board);
+
+
+/************************
+*	Static functions	*
+************************/
+
+/**
+ * Grid constructor
+ * @return (t_box **) Full size grid for the game, with boxes pointing to NULL
+ */
+static t_box** gridConstruct();
+
+
+
+#endif
+
+
