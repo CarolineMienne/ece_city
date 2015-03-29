e2910cf51c2e585b4e2ccb7e94d9950c343d7d6e
Baltair <julbrehin@gmail.com>
2015-03-29 23:46:52 +0200

    Added minimal graph implementation for testing of other features

---
+++ src/graph.h
@@ -0,0 +1,55 @@
+#ifndef ECE_CITY_GRAPH_H
+#define ECE_CITY_GRAPH_H
+
+/****************************
+*	 LIBRARY INCLUSIONS		*
+****************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "utility.h"
+
+/****************************
+*	STRUCTURE DECLARATION	*
+****************************/
+
+typedef struct graph
+{
+	char** adjacency_matrix;
+	int nb_edges;
+	t_coord** edges_coord;
+}t_graph;
+
+/****************************
+*	CONSTANT DECLARATIONS	*
+****************************/
+
+/****************************
+*	FUNCTION DECLARATIONS	*
+****************************/
+
+/** GRAPH CONSTRUCTOR/DESTRUCTOR **/
+
+/**
+ * Graph constructor
+ * @return (t_graph*) allocated and initialized empty graph
+ */
+t_graph* graphConstruct();
+/**
+ * Graph destructor
+ * @param (t_graph*) graph to be destroyed
+ */
+void graphDestroy(t_graph* graph);
+
+/** GRAPH GETTERS **/
+/**
+ * Retrieves the number of edges on the graph
+ * @param	(t_graph*) graph
+ * @return	(int) number of edges
+ */
+int getGraphNbEdges(t_graph* graph);
+
+/** GRAPH SETTERS **/
+
+#endif
