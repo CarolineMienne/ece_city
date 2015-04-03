3d3d7dce0ca6cf09cbcc76ad4938d8bfe7751b67
Baltair <julbrehin@gmail.com>
2015-04-03 22:44:24 +0200

    Ajout du module habitation
    + Définition de la structure t_habit
    + Ajout des constucteur/destructeur (vides) des habitations
    + Ajout de l'entête 'habitation.h' au fichier 'board.h'

---
+++ src/habitation.c
@@ -0,0 +1,28 @@
+#include "habitation.h"
+
+/************************************
+*	STATIC FUNCTIONS DECLARATIONS	*
+************************************/
+
+/********************************
+*	FUNCTION IMPLEMENTATIONS	*
+********************************/
+
+/************************
+* 	Global functions	*
+*************************/
+
+/** HABITATION CONSTRUCTOR/DESTRUCTOR **/
+/** Habitation constructor */
+t_habit* habitationConstruct()
+{
+	t_habit* habitation = NULL;
+
+
+	return habitation;
+}
+/** Habitation destructor */
+void habitationDestroy(t_habit* habitation)
+{
+
+}
